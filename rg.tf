329267c2b56fb8901857e1e3ad1952cebb76feb0
abhipandit1990 <abhipandit.1990@gmail.com>
2026-05-26 22:59:07 +0530

    Update rg.tf
    add new RG in existing file.

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "rg12"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example" {
+  name     = "rg2"
+  location = "centralindia"
+}
